cb1fae9360ae198c3c507dc3eab634f702ba50c7
Tim Chumbash <posseslash@gmail.com>
2025-11-30 14:38:20 +0000

    second

--- ft_printf.c
+++ ft_printf.c
@@ -6,18 +6,25 @@
 /*   By: tchumbas <tchumbas@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 18:06:09 by tchumbas          #+#    #+#             */
-/*   Updated: 2025/11/30 14:24:14 by tchumbas         ###   ########.fr       */
+/*   Updated: 2025/11/30 14:38:00 by tchumbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 static int output_id(const char *format, va_list args)
 {
 	int len;
 
 	len = 0;
-
+	if (!args)
+		return(1);
+	if (*format == 'c')
+		return(len);
+	else if (*format == '%')
+		return(len);
+	return(len);
 }
 
 int		ft_printf(const char *format, ...)
